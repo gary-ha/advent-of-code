d744217c716e745f9f2008336985a85212ebfadf
Gary Ha <gary.ha1@sainsburys.co.uk>
2021-12-06 14:55:58 +0000

    day2

--- 2021/day2/solution2b.py
+++ 2021/day2/solution2b.py
@@ -3,16 +3,17 @@ def main():
         lines = [line.rstrip('\n') for line in f]
         print(lines)
 
-    horizontal = depth = 0
+    horizontal = depth = aim = 0
+
     for i in lines:
         split_list = i.split()
         if split_list[0] == "forward":
             horizontal += int(split_list[1])
+            depth += int(split_list[1]) * aim
         if split_list[0] == "down":
-            depth += int(split_list[1])
+            aim += int(split_list[1])
         if split_list[0] == "up":
-            depth -= int(split_list[1])
-
+            aim -= int(split_list[1])
     print(horizontal * depth)
 
 
